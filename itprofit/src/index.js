7a60917a7ca5c9a2a8bd1007773e650bf8c4066d
gru6 <iliagrushevich@gmail.com>
2023-10-29 01:10:39 +0300

    refactor: implement validation

--- itprofit/src/index.js
+++ itprofit/src/index.js
@@ -4,47 +4,70 @@ import "./style.sass";
 Inputmask({ mask: "+375(99) 999-99-99" }).mask(
   document.getElementById("phone")
 );
-const inputPhone = document.getElementById("phone");
 
 document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault(); // Предотвращаем отправку формы по умолчанию
 
   // Получаем значения полей формы
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const message = document.getElementById("message").value;
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const message = document.getElementById("message");
+
+  const generalError = document.getElementById("generalError");
+  const nameError = document.getElementById("nameError");
+  const emailError = document.getElementById("emailError");
+  const phoneError = document.getElementById("phoneError");
 
   // Проверка, что поля не пустые
   if (
-    username.trim() === "" ||
-    email.trim() === "" ||
-    inputPhone.inputmask.unmaskedvalue().length < 1 ||
-    message.trim() === ""
+    username.value.trim() === "" ||
+    email.value.trim() === "" ||
+    phone.inputmask.unmaskedvalue().length < 1 ||
+    message.value.trim() === ""
   ) {
-    alert("Пожалуйста, заполните все поля.");
+    generalError.innerHTML = "Заполните все поля.";
     return;
+  } else {
+    generalError.innerHTML = "";
   }
 
   // Проверка корректности username
   const usernameRegex = /^.{3,}$/;
-  if (!usernameRegex.test(username)) {
-    alert("Имя должно содержать 2 слова длинее 3 букв.");
+  if (!usernameRegex.test(username.value)) {
+    notValid(username, nameError, "Имя длинее 3 букв.")
     return;
+  } else {
+    valid(username, nameError)
   }
 
   // Проверка корректности email
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    alert("Пожалуйста, введите корректный email.");
+  if (!emailRegex.test(email.value)) {
+    notValid(email, emailError, "Введите корректный email.")
     return;
+  } else {
+    valid(email, emailError)
   }
 
   // Проверка корректности phone
-  if (inputPhone.inputmask.unmaskedvalue().length < 9) {
-    alert("Пожалуйста, введите корректный номер.");
+  if (phone.inputmask.unmaskedvalue().length < 9) {
+    notValid(phone, phoneError, "Введите корректный номер.")
     return;
+  } else {
+    valid(phone, phoneError)
   }
 
   alert("Форма успешно отправлена!");
 });
+
+function notValid(input, el, errorMessage) {
+  input.classList.add('invalid');
+  el.innerHTML = errorMessage;
+}
+
+function valid(input, el) {
+  input.classList.remove('invalid');
+  input.classList.add('valid');
+  el.innerHTML = "";
+}
